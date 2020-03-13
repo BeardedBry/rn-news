a5f5a087b8d5b0b2479a462c15055a5c2aebd856
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-03-13 17:53:52 -0400

    stack

--- App.js
+++ App.js
@@ -5,10 +5,31 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+
+//https://reactnavigation.org/docs/hello-react-navigation
+
+function HomeScreen() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
