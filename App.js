b966325e8f183a58b3ed7e0aa5e659a3a034b421
Burkard <burkarbr@LAPTOP-BURKARBR.asddev.reyrey.com>
2020-03-13 17:38:49 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
