a2474aa3e82a9afc9b9e02b89181401a1e0b3922
Brian Burkard <brianburkard@gmail.com>
2020-03-15 20:55:37 -0400

    datePicker

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,14 @@
-import React, {useContext} from 'react';
-import { Button, View, Text } from 'react-native';
+import React, {useContext, useState} from 'react';
+import { Button, View, Text, TextInput } from 'react-native';
 import AlertContext from '../context/AlertsContext';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import style from '../styles/Stylesheet';
 
 function CreateScreen({navigation}) {
 
     const {alerts, setAlerts} = useContext(AlertContext);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
 
     const addAlert = (newAlert) => {
         setAlerts([...alerts, newAlert])
@@ -13,6 +17,20 @@ function CreateScreen({navigation}) {
     return (
       <View>
         <Text>Create Alert</Text>
+        <Text>Alert Title:</Text>
+        <TextInput
+            style={style.inputStyle}
+            onChangeText={text => setTitle(text)}
+            value={title}
+        />
+        <Text>Alert Body:</Text>
+        <TextInput
+            multiline
+            numberOfLines={4}
+            style={style.bodyStyle}
+            onChangeText={text => setBody(text)}
+            value={body}
+        />
         <Button
             title="Create New Alert"
             onPress={() => addAlert({title: 'test'})}
--- src/styles/Stylesheet.js
+++ src/styles/Stylesheet.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default StyleSheet.create({
     mainStyle: {
@@ -8,5 +9,12 @@ export default StyleSheet.create({
     titleStyle: {
       padding: 20, 
       fontSize: 15
+    },
+    inputStyle: {
+      backgroundColor: "white",
+      height: 40
+    },
+    bodyStyle: {
+      backgroundColor: "white",
     }
 });
