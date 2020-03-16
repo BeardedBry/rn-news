da42c726d1633412fd8daaec295ee377de5658db
Brian Burkard <brianburkard@gmail.com>
2020-03-15 21:31:12 -0400

    Date Component

---
+++ src/components/DateFields.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, View, Text, TextInput } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+function DateFields({props}){
+
+  const {date, setDate, mode, setMode, show, setShow} = props;
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+ 
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+ 
+  const showDatepicker = () => {
+    showMode('date');
+  };
+ 
+  const showTimepicker = () => {
+    showMode('time');
+  };
+ 
+  return (
+    <View style={{margin: 10}}>
+      <View style={{paddingBottom: 5}}>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          timeZoneOffsetInMinutes={0}
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </View>
+  );
+};
+
+export default DateFields;
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,20 @@
 import React, {useContext, useState} from 'react';
 import { Button, View, Text, TextInput } from 'react-native';
 import AlertContext from '../context/AlertsContext';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateFields from '../components/DateFields';
 import style from '../styles/Stylesheet';
+
 
 function CreateScreen({navigation}) {
 
     const {alerts, setAlerts} = useContext(AlertContext);
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
+
+    // for date picker
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
 
     const addAlert = (newAlert) => {
         setAlerts([...alerts, newAlert])
@@ -31,6 +37,8 @@ function CreateScreen({navigation}) {
             onChangeText={text => setBody(text)}
             value={body}
         />
+        <DateFields props={{date, setDate, mode, setMode, show, setShow}}/>
+        <Text>{date.toString()}</Text>
         <Button
             title="Create New Alert"
             onPress={() => addAlert({title: 'test'})}
